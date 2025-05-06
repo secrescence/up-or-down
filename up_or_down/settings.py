cdb1fed72fc6c8d14f9c3e555e5ee5ed01f40121
secrescence <edselpisuena@gmail.com>
2025-05-06 16:15:41 +0800

    Fix allowed hosts for production

--- up_or_down/settings.py
+++ up_or_down/settings.py
@@ -17,6 +17,7 @@ from environ import Env
 # Initialise environment variables
 env = Env()
 Env.read_env()
+ENVIRONMENT = env("ENVIRONMENT", default="development")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,10 +30,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if ENVIRONMENT == "development":
+    DEBUG = True
+else:
+    DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "secre.up.railway.app"]
 
+CSRF_TRUSTED_ORIGINS = ["https://secre.up.railway.app"]
 
 # Application definition
 
